1ddb75a4f4eb315a813eff7a2275dcc8757e3438
Slingreen <benjamindarici@hotmail.com>
2022-04-07 15:01:34 +0200

    Startup

---
+++ Source/SpellingGame/SpellingGame.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "SpellingGame.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, SpellingGame, "SpellingGame" );
